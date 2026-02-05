dba3c077ab56cb45d9ce7c8eada97ebe69fef9e6
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2026-02-05 14:45:30 -0300

    feat: add error message to the create booking dto

--- backend/src/presentation/http/bookings/dtos/create-booking.dto.ts
+++ backend/src/presentation/http/bookings/dtos/create-booking.dto.ts
@@ -1,3 +1,4 @@
+import { SupportedCurrencies } from '@shared/supported-currencies';
 import { Static, Type } from '@sinclair/typebox';
 
 import { BookingEntity } from '@domain/entities/booking/booking.entity';
@@ -5,14 +6,26 @@ import { BookingSchema } from '@presentation/docs/schemas/booking.schema';
 
 export const CreateBookingBodySchema = Type.Object(
   {
-    ticketTierId: Type.String({ format: 'uuid' }),
-    userEmail: Type.String({ format: 'email' }),
-    quantity: Type.Integer({ minimum: 1, maximum: 10 }),
-    totalPrice: Type.Number({ minimum: 0 }),
-    currency: Type.String({ minLength: 3, maxLength: 3 }),
-    idempotencyKey: Type.String({ minLength: 10 }),
+    ticketTierId: Type.String({ format: 'uuid', errorMessage: 'Invalid UUID format for ticketTierId' }),
+    userEmail: Type.String({ format: 'email', errorMessage: 'Invalid email format' }),
+    quantity: Type.Integer({ minimum: 1, maximum: 10, errorMessage: 'Quantity must be between 1 and 10' }),
+    totalPrice: Type.Number({ minimum: 0, errorMessage: 'Total price must be a positive number' }),
+    currency: Type.Enum(SupportedCurrencies, { errorMessage: 'Currency must be a valid ISO 4217 code' }),
+    idempotencyKey: Type.String({ minLength: 10, errorMessage: 'Idempotency key must be at least 10 characters long' }),
   },
-  { $id: 'CreateBookingBody' },
+  {
+    $id: 'CreateBookingBody',
+    errorMessage: {
+      required: {
+        userEmail: 'User email is required',
+        ticketTierId: 'Ticket tier ID is required',
+        quantity: 'Quantity is required',
+        totalPrice: 'Total price is required',
+        currency: 'Currency is required',
+        idempotencyKey: 'Idempotency key is required',
+      },
+    },
+  },
 );
 
 export const CreateBookingResponseSchema = Type.Object({ booking: BookingSchema }, { $id: 'CreateBookingResponse' });
