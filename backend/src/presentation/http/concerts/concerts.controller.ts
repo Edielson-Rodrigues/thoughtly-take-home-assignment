175f70a3f13460f5e454b56371a7e4075f3d9f4b
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2026-02-05 20:15:23 -0300

    feat: set header in sse route

--- backend/src/presentation/http/concerts/concerts.controller.ts
+++ backend/src/presentation/http/concerts/concerts.controller.ts
@@ -17,6 +17,9 @@ export class ConcertsController {
   }
 
   stream(req: FastifyRequest, reply: FastifyReply) {
+    reply.raw.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+    reply.raw.setHeader('Access-Control-Allow-Credentials', 'true');
+
     reply.raw.setHeader('Content-Type', 'text/event-stream');
     reply.raw.setHeader('Cache-Control', 'no-cache');
     reply.raw.setHeader('Connection', 'keep-alive');
