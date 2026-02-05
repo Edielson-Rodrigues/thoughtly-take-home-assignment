a3114acfb932b1417d96a89935b155483e3664e4
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2026-02-05 14:46:48 -0300

    chore: improve project's bootstrap

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,28 @@
 import cors from '@fastify/cors';
+import { Logger } from '@shared/logger';
+import ajvErrors from 'ajv-errors';
 import Fastify from 'fastify';
 
 import { CacheProvider } from '@cache/cache.provider';
 import { ConfigService } from '@config/config.service';
-import { httpErrorHandler } from '@core/middlewares/http-error.middleware';
 import { DatabaseProvider } from '@database/database.provider';
 import { buildDocs } from '@presentation/docs/build-docs';
 import { httpRoutes } from '@presentation/http/http.routes';
-
-import { Logger } from './shared/logger';
 
 const createApp = (configService: ConfigService) => {
   const { environment } = configService.get('app');
   const logger = Logger.initialize({ environment });
 
-  return { app: Fastify({ loggerInstance: logger }), logger };
+  return {
+    app: Fastify({
+      loggerInstance: logger,
+      ajv: {
+        customOptions: { allErrors: true },
+        plugins: [ajvErrors as any],
+      },
+    }),
+    logger,
+  };
 };
 
 const connectInfra = async (configService: ConfigService) => {
@@ -30,12 +38,10 @@ const bootstrap = async () => {
     const { port } = configService.get('app');
 
     await connectInfra(configService);
-    await buildDocs(app, configService);
+    await buildDocs(app);
 
     await app.register(cors, { origin: true });
     await app.register(httpRoutes, { prefix: '/api' });
-
-    app.setErrorHandler(httpErrorHandler);
 
     await app.listen({ port, host: '0.0.0.0' });
 
