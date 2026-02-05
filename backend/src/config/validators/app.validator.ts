8582802b8daa0dfc4be0e5bf321bc8940bc7656b
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2026-02-05 01:01:25 -0300

    feat: update appSchema

--- backend/src/config/validators/app.validator.ts
+++ backend/src/config/validators/app.validator.ts
@@ -2,10 +2,12 @@ import z from 'zod';
 
 const appEnvSchema = z.object({
   API_PORT: z.coerce.number().optional().default(8000),
+  ENVIRONMENT: z.enum(['development', 'production']).optional().default('production'),
 });
 
 export const appSchema = appEnvSchema.transform((env) => ({
   port: env.API_PORT,
+  environment: env.ENVIRONMENT,
 }));
 
 export type AppConfig = z.infer<typeof appSchema>;
